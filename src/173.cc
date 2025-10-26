4b365189cb2305a2e22269ba588adcb3fa7d3488
Richard-Qin-001 <69390919+Richard-Qin-001@users.noreply.github.com>
2025-10-27 01:13:19 +0800

    173 Finished

--- src/173.cc
+++ src/173.cc
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <vector>
-
+// 布尔数组
 using namespace std;
 
-void solve(){
-    int M, n;
-    cin >> M >> n;
-    vector<vector<int>> matrix;
-    vector<int> tmp(2);
-    cin >> tmp[0] >> tmp[1];
-    matrix.push_back(tmp);
-    int index = 0;
-    for (int i = 1; i < n; i++){
-        cin >> tmp[0] >> tmp[1];
-        if (tmp[0] <= matrix[index][0] && tmp[1] >= matrix[index][1]) matrix[index] = tmp;
-        else if (tmp[0] <= matrix[index][0] && tmp[1] <= matrix[index][1]) matrix[index][0] = tmp[0];
-        else if (tmp[0] >= matrix[index][0] && tmp[1] >= matrix[index][1]) matrix[index][1] = tmp[1];
-        else if (tmp[0] >= matrix[index][0] && tmp[1] <= matrix[index][1]) continue;
-        else {
-            matrix.push_back(tmp);
-            index++;
-        }
-    }
+void remove(vector<bool>&tree_removed, const int&l, const int&r){
+    for(int i = l; i <= r; i++) tree_removed[i] = true;
 }
+
+void solve(){
+    int L, M;
+    cin >> L >> M;
+    vector<bool> tree_removed(L+1, false);
+    for(int i = 0; i < M; i++){
+        int l,r;
+        cin >> l >> r;
+        remove(tree_removed, l, r);
+    }
+    int left_trees = 0;
+    for(int i = 0; i <= L; i++)if(!(tree_removed[i])) left_trees++;
+    cout << left_trees;
+}
+int main(){solve(); return 0;}
