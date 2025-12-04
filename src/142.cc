8ad024aea7700636bee08ae663765b42e4af8dfd
Richard-Qin-001 <69390919+Richard-Qin-001@users.noreply.github.com>
2025-12-04 21:06:14 +0800

    142

--- src/142.cc
+++ src/142.cc
@@ -11,23 +11,16 @@ void solve(){
     for (int i = 0; i < k; i++) cin >> sort_columns[i];
     vector<vector<int>> matrix(m, vector<int>(n, 0));
     for(int i = 0; i < m; ++i)for(int j = 0; j < n; ++ j)cin >> matrix[i][j];
-    sort(
-        matrix.begin(), matrix.end(),
-        [&sort_columns](const vector<int>& row1, const vector<int>& row2){
-            for(auto&col1:sort_columns){
-                int col0 = col1-1;
-                if (row1[col0] != row2[col0]){
-                    return row1[col0] < row2[col0];
-                }
-            }
+    sort(matrix.begin(), matrix.end(),[&sort_columns](const vector<int>& row1, const vector<int>& row2){
+        for(auto&col1:sort_columns){
+            int col0 = col1-1;
+            if (row1[col0] != row2[col0])return row1[col0] < row2[col0];  
+        }
         return row1[0] < row2[0];
-        }
-    );
+        });
     for (const auto& row : matrix) {
-        for (int i = 0; i < row.size(); ++i) {
-            cout << row[i];
-            if (i < row.size() - 1) cout << " "; 
-        }
+        int len = row.size();
+        for (int i = 0; i < len; ++i) cout << row[i] << ((i != len - 1) ? " " : "");
         cout << endl;
     }
 }
