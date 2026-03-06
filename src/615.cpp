428e241065558ab4283a2fa93516f724cdefb176
Richard-Qin-001 <69390919+Richard-Qin-001@users.noreply.github.com>
2026-03-07 02:58:18 +0800

    615

---
+++ src/615.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	// ____qcodep____
+    cout << "C" << endl;
+    cout << "C" << endl;
+    cout << "A" << endl;
+    cout << "A" << endl;
+    cout << "C" << endl;
+    cout << "A" << endl;
+    cout << "B" << endl;
+    cout << "E" << endl;
+    cout << "C" << endl;
+    cout << "H" << endl;
+    cout << "E" << endl;
+    cout << "F" << endl;
+    cout << "B" << endl;
+    cout << "E" << endl;
+}
